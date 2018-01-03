ce4e9d4d886e7c387221877ea4703a8c0e170c07
Philippe Hässig <phil@nine.ch>
2018-01-03 09:27:37 +0100

    Add aliases for fish

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -3,3 +3,6 @@ alias dct "docker-compose -f docker-compose.test.yml"
 
 alias bi "bundle install"
 alias be "bundle exec"
+
+alias got "go test"
+alias gor "go run"
--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -1,5 +1,8 @@
 
 set EDITOR nvim
 
+# set the workspace path
 set -x GOPATH $HOME/src/go
-set -x PATH $GOPATH/bin $PATH
+
+# add the go bin path to be able to execute our programs
+set -x PATH $PATH $GOPATH/bin
