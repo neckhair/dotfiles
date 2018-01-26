209a927e6beda997ee501348e41ec31f568aead7
Philippe Hässig <phil@nine.ch>
2018-01-26 10:21:29 +0100

    Add more variables

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -6,3 +6,7 @@ set -x GOPATH $HOME/src/go
 
 # add the go bin path to be able to execute our programs
 set -x PATH $PATH $GOPATH/bin
+
+set -x PATH $HOME/bin $PATH
+
+set -x TF_VAR_nine_mnt_ssh_user phil
