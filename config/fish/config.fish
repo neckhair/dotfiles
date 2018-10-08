baf143d2a92ad9ff00c44d42c3e372cc3c4573ab
Philippe Hässig <phil@nine.ch>
2018-10-08 16:17:55 +0200

    add sbin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,5 @@ source ~/.config/fish/variables.fish
 source ~/.config/fish/aliases.fish
 
 status --is-interactive; and source (rbenv init -|psub)
+
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
