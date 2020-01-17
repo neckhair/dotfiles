57231677cde6b56d276b4af7fa1584e46fc9af1a
Philippe Hässig <phil@nine.ch>
2020-01-17 13:06:49 +0100

    Add kubectl alias

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,5 +1,4 @@
 alias dc "docker-compose"
-alias dct "docker-compose -f docker-compose.test.yml"
 
 alias bi "bundle install"
 alias be "bundle exec"
@@ -10,3 +9,5 @@ alias gor "go run"
 alias av "ansible-vault --vault-password-file=.vault"
 
 alias gpr "git pull --rebase"
+
+alias kc "kubectl"
