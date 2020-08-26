9605075ae06a4a9188a125ece41a725d52d1c77f
Philippe Hässig <phil@nine.ch>
2020-08-26 09:43:00 +0200

    Add some fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,5 @@ source ~/.config/fish/aliases.fish
 status --is-interactive; and source (rbenv init -|psub)
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/postgresql@9.6/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/helm@2/bin" $fish_user_paths
