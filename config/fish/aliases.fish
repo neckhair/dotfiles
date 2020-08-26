cce14ef59510f99e2c2a125d4c11947019bbfe77
Philippe Hässig <phil@nine.ch>
2020-08-26 09:41:09 +0200

    Add some fish aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -11,3 +11,7 @@ alias av "ansible-vault --vault-password-file=.vault"
 alias gpr "git pull --rebase"
 
 alias kc "kubectl"
+alias kns "kubens"
+
+# run curl on kubernetes
+alias kubecurl "kubectl run busybox -ti --rm --image=radial/busyboxplus:curl --restart=Never -- curl"
